ab9b5455b5f92e49cd66483ad3868dd315422591
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2023-10-16 12:22:37 +0200

    javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction(){
+
+    var variable1; // Nebudeme použivat !
+    let variable2 // budeme použivat !
+    const variable3 = 3; // nemohu poté zmenit 
+    variable4 = "ahoj" // STRING
+    variable5 = 5 // NUMBER/INT/INTEGER
+    variable6 = true // BOOLEAN
+
+    const userFirstName = document.getElementById("firstName"). value
+    const userFirstName = document . getElementById ("firstName"). value
+
+    const userFirstName  = userFirstName + " " + userLastName 
+
+    console.log(userFirstName)
+    
+}
