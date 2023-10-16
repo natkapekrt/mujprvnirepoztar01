fda1221629f435526ffe81b800bad7220ddfd810
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2023-10-16 15:05:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,29 @@ function myFunction(){
     variable5 = 5 // NUMBER/INT/INTEGER
     variable6 = true // BOOLEAN
 
-    const userFirstName = document.getElementById("firstName"). value
-    const userFirstName = document . getElementById ("firstName"). value
-
-    const userFirstName  = userFirstName + " " + userLastName 
-
+    const userFirstName=document.getElementById("firstName").value 
     console.log(userFirstName)
+
+    const userLastName=document.getElementById("lastName").value
+    const fullName= userFirstName + " " + userLastName
+ 
+    document.getElementById("fullName").value=addtionResult
     
 }
+
+function addition() { 
+    const inputFirstNumber=document.getElementById("firstName").value
+    const inputSeconNumber=document.getElementById("seconName").value
+
+    document.getElementById("result").value=addtionResult
+
+}
+ 
+function substracion () { 
+    const inputFirstNumber=parseInt (document.getElementById("firstNumber").value
+    const inputSeconNumber=parseInt (document.getElementById("seconNumber").value
+
+    const addtionResult = inputFirstNumber + inputSeconNumber
+
+    document.getElementById("result").value=addtionResult
+
