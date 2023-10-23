1b25a9bbdbfbce309691abaed20e24b9b0a6aef1
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2023-10-23 12:20:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-function myFunction(){
+function getName(){
 
-    var variable1; // Nebudeme použivat !
-    let variable2 // budeme použivat !
-    const variable3 = 3; // nemohu poté zmenit 
-    variable4 = "ahoj" // STRING
-    variable5 = 5 // NUMBER/INT/INTEGER
-    variable6 = true // BOOLEAN
-
-    const userFirstName=document.getElementById("firstName").value 
-    console.log(userFirstName)
-
-    const userLastName=document.getElementById("lastName").value
-    const fullName= userFirstName + " " + userLastName
- 
-    document.getElementById("fullName").value=addtionResult
-    
 }
+function checkResult (){
+    const inputNumber = parseInt (document. getElementById (""))
 
-function addition() { 
-    const inputFirstNumber=document.getElementById("firstName").value
-    const inputSeconNumber=document.getElementById("seconName").value
-
-    document.getElementById("result").value=addtionResult
+    if(inputNumber < 10 ) {
+        consol.log("MENŠÍ NEŽ 10")
+     
 
 }
  
-function substracion () { 
-    const inputFirstNumber=parseInt (document.getElementById("firstNumber").value
-    const inputSeconNumber=parseInt (document.getElementById("seconNumber").value
-
-    const addtionResult = inputFirstNumber + inputSeconNumber
-
-    document.getElementById("result").value=addtionResult
-
