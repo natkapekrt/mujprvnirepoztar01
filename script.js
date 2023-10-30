adf478fef1dc1767a8a031d47b6ad1070a9173af
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2023-10-30 14:54:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 
-function pythagorovavěta() {
+function checkResult() {
 
-    const inputFirstNumber = parseInt(document.getElementById("firstNumber").value)
-    const inputSecondNumber = parseInt(document.getElementById("secondNumber").value)
+    const inputNumber = parseInt(document.getElementById("numberCheck").value)
 
-    const additionResult = (inputFirstNumber * inputFirstNumber + inputSecondNumber * inputSecondNumber)
-        
-    let C =Math.sqrt(a*a + b*b)
-    document.getElementById("c").value = c.toFixed(1)
+if(inputNumber => 18) {
+    console.log("Dospělý")
+  }else if (inputNumber < 18)
+    console.log("Mladý")
 
-    }
+}
+function myNumber(){
+ 
+    const inputAge = document.getElementById("num").value;
+ 
+    if(inputAge >= 18){
+    console.log("jste dospělý")
+}  else if(inputAge < 18){
+    console.log("jste ještě dítě")
+}
+}
+ 
+function myNumber1(){
+ 
+    const inputAge1 = document.getElementById("num1").value;
+ 
+    if(inputAge1 <= 2005){
+    console.log("přistup povolen")
+}  else if(inputAge1 > 2005){
+    console.log("přístup zamítnut")
+}
+}
+ 
