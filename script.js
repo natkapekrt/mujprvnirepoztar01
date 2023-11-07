596c4c56f8f4d736044bd184d2b88660bc1987a7
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2023-11-07 13:12:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
+
+const usersData = []
 function checkResult() {
  
-    const weight = parseFloat(document.getElementById("weight").value)
-    const height = parseFloat(document.getElementById("height").value)
+    const weightInput = parseFloat(document.getElementById("weight").value)
+    const heightInput = parseFloat(document.getElementById("height").value)
  
-    BMI = weight / ((height * height)).toFixed(1)
-      console.log(weight, height, BMI)
- 
-    if(BMI < 18.5) {
-        document.getElementById("outputWeight").value = "Podváha"
-   
+     const BMI = weightInput / ((heightInput * heightInput)).toFixed(1)
+
+     if(weightInput<= 0){
+     document.getElementById("outputWeight").innerHTML = "Záporná hodnota"
+        usersData.push("zaporná hodnota")
+    }else if(heightInput<= 0){
+        document.getElementById("outputWeight").innerHTML = "Záporná hodnota"
+    }else if( height <= 18.5 && height < 25){
+
+     if(BMI < 18.5) {
+        document.getElementById("outputWeight").innerHTML = "Podváha"
+         usersData.push("podváha")
+
     } else if( BMI <= 18.5 && BMI < 25){
-        document.getElementById("outputWeight").value = "Normální váha"
- 
+        document.getElementById("outputWeight").innerHTML = "Normální váha"
+        usersData.push("normální váha")
+
     }else if(  BMI <= 25 && BMI < 30){
-        document.getElementById("outputWeight").value = "Nadváha"
- 
-    } else if( BMI > 30){
-        document.getElementById("outputWeight").value = "Obezita"
-    }
-   
-}
+        document.getElementById("outputWeight").innerHTML = "Nadváha"
+        usersData.push("nadváha")
+
+    } else if( BMI> 30)
+        document.getElementById("outputWeight").innerHTML = "Obezita"
+        usersData.push("obezita")
+   }
+
+} 
+
