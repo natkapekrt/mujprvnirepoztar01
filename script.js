d9b9580d36d943b6ab589aa63a54d7d05a637726
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2023-11-13 11:17:29 +0100

    BMI Měření

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-
-const usersData = []
-function checkResult() {
+const usersData =[]
  
-    const weightInput = parseFloat(document.getElementById("weight").value)
-    const heightInput = parseFloat(document.getElementById("height").value)
+ function calculatorBMI() {
+     
+        const weightInput = parseFloat(document.getElementById("weight").value)
+        const heightInput = parseFloat(document.getElementById("height").value)
  
-     const BMI = weightInput / ((heightInput * heightInput)).toFixed(1)
-
-     if(weightInput<= 0){
-     document.getElementById("outputWeight").innerHTML = "Záporná hodnota"
-        usersData.push("zaporná hodnota")
-    }else if(heightInput<= 0){
+     
+        const BMI = weightInput / ((heightInput*heightInput)).toFixed(1)
+ 
+        console.log(BMI)
+     
+    if(weightInput <= 0){
         document.getElementById("outputWeight").innerHTML = "Záporná hodnota"
-    }else if( height <= 18.5 && height < 25){
-
-     if(BMI < 18.5) {
-        document.getElementById("outputWeight").innerHTML = "Podváha"
-         usersData.push("podváha")
-
+        usersData.push("zaporná hodnota")
+ 
+    }else if(heightInput <= 0){
+        document.getElementById("outputWeight").innerHTML = "Záporná hodnota"
+    }else if (weightInput > 0 && heightInput > 0){
+       
+    if(BMI < 18.5) {
+       document.getElementById("outputWeight").innerHTML = "Podváha"
+        usersData.push("podváha")
+ 
     } else if( BMI <= 18.5 && BMI < 25){
-        document.getElementById("outputWeight").innerHTML = "Normální váha"
-        usersData.push("normální váha")
-
-    }else if(  BMI <= 25 && BMI < 30){
-        document.getElementById("outputWeight").innerHTML = "Nadváha"
-        usersData.push("nadváha")
-
-    } else if( BMI> 30)
-        document.getElementById("outputWeight").innerHTML = "Obezita"
-        usersData.push("obezita")
-   }
-
-} 
-
+            document.getElementById("outputWeight").innerHTML = "Normální váha"
+     usersData.push("normální váha")
+ 
+    } else if(  BMI <= 25 && BMI < 30){
+            document.getElementById("outputWeight").innerHTML = "Nadváha"
+     usersData.push("nadváha")
+ 
+    } else if( BMI > 30){
+            document.getElementById("outputWeight").innerHTML = "Obezita"
+            usersData.push("obezita")
+        }
+    }
+ 
+console.log ()
+    document.getElementById("arrayOutput").innerHTML = usersData
+ }
+ 
+   
